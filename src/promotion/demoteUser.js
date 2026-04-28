ed8455c5716a1d91c6591d74cc3929b2e5eeebbb
Spoderman570 <synjonelder@gmail.com>
2026-04-28 03:04:11 +0000

    commands deployment fixed

--- src/promotion/demoteUser.js
+++ src/promotion/demoteUser.js
@@ -1,31 +1,50 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
-const demoteUser = require("../promotion/demoteUser");
-const { teams } = require("../promotion/teams");
+const { EmbedBuilder } = require("discord.js");
+const { teams } = require("./teams");
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("demote")
-        .setDescription("Demote a user within their team.")
-        .addUserOption(opt =>
-            opt.setName("user").setDescription("User to demote").setRequired(true)
-        )
-        .addStringOption(opt =>
-            opt.setName("team")
-                .setDescription("Team to demote within")
-                .setRequired(true)
-                .addChoices(
-                    ...Object.keys(teams).map(key => ({
-                        name: teams[key].name,
-                        value: key
-                    }))
-                )
-        )
-        .addStringOption(opt =>
-            opt.setName("reason").setDescription("Reason for demotion").setRequired(true)
-        )
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
+module.exports = async function demoteUser(interaction) {
+    const user = interaction.options.getMember("user");
+    const teamKey = interaction.options.getString("team");
+    const reason = interaction.options.getString("reason");
 
-    async execute(interaction) {
-        demoteUser(interaction);
+    const guild = interaction.guild;
+    const team = teams[teamKey];
+
+    if (!team) {
+        return interaction.reply({ content: "Invalid team selected.", ephemeral: true });
     }
+
+    const categoryRole = guild.roles.cache.get(team.categoryRole);
+    const rankRoles = team.ranks.map(r => guild.roles.cache.find(role => role.name === r));
+
+    let currentRankIndex = rankRoles.findIndex(role => user.roles.cache.has(role?.id));
+
+    // If user has category role, demote to highest rank
+    if (user.roles.cache.has(categoryRole.id)) {
+        user.roles.remove(categoryRole);
+        if (rankRoles.length > 0) {
+            user.roles.add(rankRoles[rankRoles.length - 1]);
+        }
+    } else if (currentRankIndex > 0) {
+        // Demote to previous rank
+        user.roles.remove(rankRoles[currentRankIndex]);
+        user.roles.add(rankRoles[currentRankIndex - 1]);
+    } else {
+        return interaction.reply({
+            content: "User is already at the lowest rank in this team.",
+            ephemeral: true
+        });
+    }
+
+    const embed = new EmbedBuilder()
+        .setColor(0xff6b6b)
+        .setTitle("User Demoted")
+        .addFields(
+            { name: "User", value: `${user}`, inline: true },
+            { name: "Team", value: team.name, inline: true },
+            { name: "Moderator", value: `${interaction.user}`, inline: true },
+            { name: "Reason", value: reason }
+        )
+        .setTimestamp();
+
+    await interaction.reply({ embeds: [embed] });
 };
